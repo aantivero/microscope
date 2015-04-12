10ec7c5a845a98163f73e33960b54277a7c181ef
Alejandro Antivero <alejandro.antivero@gmail.com>
2015-04-11 22:55:04 -0300

    4-3 conexión entre la colección Posts y la plantilla 'postsList'

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,20 +1,8 @@
 /**
  * Created by alex on 11/04/2015.
  */
-var postsData = [
-    {
-        title: 'Introducing Telescopre',
-        url: 'http://sachagreif.com/introducing-telescope'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function () {
+        return Posts.find();
+    }
 });
