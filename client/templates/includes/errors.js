5b2c29a8945c357b552be7daddd9060e0fa83790
Alejandro Antivero <alejandro.antivero@gmail.com>
2015-04-14 11:45:51 -0300

    9-3 limpiar errores después de 3 segundos luego del renderizado de la plantilla

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -7,3 +7,15 @@ Template.errors.helpers({
         return Errors.find();
     }
 });
+
+//vamos a eliminar de la colección los antiguos errores
+//vamos hacer una función que se ejecute después de 3000 milisegundos
+//la llamada rendered se llama una vez que la plantilla ha sido renderizada
+Template.error.rendered = function () {
+    //this hace referencia a la instancia actual de la plantilla
+    //this.data nos da acceso a los datos del objeto que está siendo renderizado
+    var error = this.data;
+    Meteor.setTimeout(function () {
+        Errors.remove(error._id);
+    }, 3000);
+};
