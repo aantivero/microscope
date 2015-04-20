0eed21a02ffa0aa9239bc64fbfbec5d8fc335818
Alejandro Antivero <alejandro.antivero@gmail.com>
2015-04-19 22:01:52 -0300

    13-2 Deshabilito la posibilidad de votar un post si el usuario no esta registrado o ya ha votado ese post

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -10,10 +10,19 @@ Template.postItem.helpers({
         var a = document.createElement('a');
         a.href = this.url;
         return a.hostname;
+    },
+    //si no esta conectado o ya ha votado no podrà votar, condicional css para deshabilitar el botón
+    upvotedClass: function () {
+        var userId = Meteor.userId();
+        if (userId && !_.include(this.upvoters, userId)) {
+            return 'btn-primary upvotable';
+        } else {
+            return 'disabled';
+        }
     }
 });
 Template.postItem.events({
-   'click .upvote': function (e) {
+   'click .upvotable': function (e) {
        e.preventDefault();
        Meteor.call('upvote', this._id);
    }
